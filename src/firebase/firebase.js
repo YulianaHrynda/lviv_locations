9e8c6dc215e12eda8208eeaaac695d6d338f540d
Iryna Denysova <denysova.pn@ucu.edu.ua>
2025-05-19 02:58:29 +0300

    firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,20 +1,20 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
+// firebase.js
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBJJ3XFcBJf5JfKGtOH2C5RcQB-chacT2w",
-  authDomain: "lviv-sightseeings.firebaseapp.com",
-  projectId: "lviv-sightseeings",
-  storageBucket: "lviv-sightseeings.appspot.com",
-  messagingSenderId: "543880476813",
-  appId: "1:543880476813:web:b769b3d174c852dc1f0ac4",
-  measurementId: "G-B8F6ZXSNY9"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-
 const db = getFirestore(app);
-const analytics = typeof window !== "undefined" ? getAnalytics(app) : null;
+const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null;
 
 export { db, analytics };
